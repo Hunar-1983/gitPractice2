1b9ecaf475c450eb16f2416f0d87288c72dc7198
Hunar-1983 <parezarhunar@gmail.com>
2020-04-24 15:51:14 -0400

    add B18.java

---
+++ src/day_1/ab.java
@@ -0,0 +1,9 @@
+package day_1;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class ab {
+    public static void main(String[] args) {
+        System.out.println("B 18");
+    }
+}
